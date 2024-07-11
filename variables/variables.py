dc967f5c50c5c9fa4d3a0fca134a341349646d66
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-07-10 21:01:51 -0500

    trabajamos variables

---
+++ variables/variables.py
@@ -0,0 +1,11 @@
+nombre="ricardo"
+bienvenida= f"hola {nombre} feliz noche"
+
+
+print(bienvenida)
+print ("ola" in bienvenida)
+print("betty " not in bienvenida)
+
+print("hola" not in bienvenida)
+
+nombre_completo_de_tu_profesor="jheyson Galvis"
