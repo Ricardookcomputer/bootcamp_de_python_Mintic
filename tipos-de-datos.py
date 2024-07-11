cb2db9012e384f904357111ae32e63d9fb15590c
RADIOHEAD-OK COMPUTER <Ricardomaldonadocamargo1@gmail.com>
2024-07-10 19:31:32 -0500

    Update tipos-de-datos.py

---
+++ tipos-de-datos.py
@@ -0,0 +1,13 @@
+"string"
+"""Tus dato son
+nombre: Ricardo
+apellidos: Maldonado
+
+"""
+# tipos de datos numericos
+4#tipos de datos numericos
+4.5 #tipos de daos flotantes
+
+# tipos de das booleanos
+True#tre van con T mayuscula
+False#False van con F  mayuscula
