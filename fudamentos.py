9b6b5c7fd852a35a9a9ef6ff8d1e93bd9460143c
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-07-15 19:49:48 -0500

    se abordaron los documentos de python

---
+++ fudamentos.py
@@ -0,0 +1,11 @@
+name="Ricardo"
+print(type(name))
+print(name[0])
+print(name[1])
+last_name="Maldonado Camargo"
+print(name*5)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name + " "+ last_name)
