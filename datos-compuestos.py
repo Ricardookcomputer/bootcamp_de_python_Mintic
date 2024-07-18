6a9d658cd4db237f2d698be66c38a116449aca16
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-07-17 19:16:45 -0500

    abordamos  datos compuestos

---
+++ datos-compuestos.py
@@ -0,0 +1,36 @@
+# el primer  tipo de datos  que veremos  sera la lista
+
+lista=["Ricardo Maldonado","tecnotutoriales Jheyson Galvis",True ,1.80]
+print(lista[1])
+lista[3]="Richard"
+print(lista[3])
+
+# la tupla  es una lista que se no se puede modificar
+tupla=("Ricardo Maldonado","tecnotutoriales Jheson Galvis",True ,1.80)
+print(tupla[1])
+
+#tupla[1]="TIC al paso"
+#print(tupla[1])
+
+#creando un conjunto  o set
+#un conjunto no admite elementos repetidos
+conjunto={"Ricardo Maldonado","tecnotutoriales Jheson Galvis",True ,1.80,1.80}
+print(conjunto)
+
+set={"Ricardo Maldonado","tecnotutoriales Jheson Galvis",True ,1.80,1.80}
+print(set)
+
+# creando un diccionario 
+diccionario={
+    "nombre": "Ricardo",
+    "apellido" :"maldonado",
+    "estatura": 1.80,
+    "estanfeliz":True
+}
+print(diccionario["esta feliz"])
+print(diccionario["nombre"])
+print(diccionario["apellido"])
+print(diccionario["estatura"])
+    
+    
+
