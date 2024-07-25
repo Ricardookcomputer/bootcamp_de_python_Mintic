06bc1fc8daf82a8e09b9af797628a0a4eb584c59
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-07-24 19:18:05 -0500

    abordamos operadores de comparacion

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,44 @@
+
+
+es_igual_que = 5 == 5
+
+print(es_igual_que)
+
+es_diferente = 5 != 6
+
+print(es_diferente)
+
+es_mayor_que = 5 > 6
+
+print(es_mayor_que)
+
+es_mayor_igual_que = 5 >= 6
+
+print(es_mayor_igual_que)
+
+es_menor_que = 5 < 6
+
+print(es_menor_que)
+
+es_menor_igual_que = 5 <= 6
+
+print(es_menor_igual_que)
+
+
+
+a=5
+b=10
+c=20
+comparacion=a+b==c
+print(comparacion)
+contraseña_almacenada="123456"
+contraseña_escrita="123456"
+print(contraseña_almacenada==contraseña_escrita)
+
+contraseña_almacenada="123456"
+contraseña_escrita='123456'
+print(contraseña_almacenada==contraseña_escrita)
+
+contraseña_almacenada="123456"
+contraseña_escrita='''123456'''
+print(contraseña_almacenada==contraseña_escrita)
