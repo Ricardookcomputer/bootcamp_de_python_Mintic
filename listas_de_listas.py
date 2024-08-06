e0926d914d012929eb9489ea885207c56388aed3
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-08-05 20:58:59 -0500

    creamos lista de listas

---
+++ listas_de_listas.py
@@ -0,0 +1,13 @@
+matrix=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+print(matrix)
+print(matrix[0])
+
+numbers=(1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+
+print(numbers[0])
+#numbers[0]=10
