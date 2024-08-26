e2544fec4425b0c9f7158b4dd3c72f4390b41fa1
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-08-26 18:59:29 -0500

    realizamos bucles y iteraciones

---
+++ bucles_y_contrtol_de_iteraciones.py
@@ -0,0 +1,26 @@
+numbers= [1,2,3,4,5,6]
+for i in numbers:
+    print( " aqui i es igual a :",i+1)
+    for i in  range(3,10):
+     print(i)
+     
+     fruits =["Manzana","pera","uva","Naranja","kiwi"]
+     for frutas in fruits:
+         
+      print(frutas)
+    if frutas=="Naranja":
+             print("naranja encontrada")
+x=0
+while x<5:
+  if x==3:
+      break                          
+                 
+  print(x)
+  x+=1
+
+numbers= [1,2,3,4,5,6]
+for i in numbers:
+    if i == 3:
+        break
+    print("aqui i  es igual a :",i+1)
+
