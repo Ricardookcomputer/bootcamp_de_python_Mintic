733831c87541aba488f695c402416ab39a63a880
Ricardo Maldonado <ricardomaldonadocamargo1@gmail.com>
2024-09-02 20:04:30 -0500

    uso de funciones

--- iteradores.py
+++ iteradores.py
@@ -1,18 +1,18 @@
-# como funcionan losmiteradores
+'''# como funcionan losmiteradores
 # crear un alista de con algunos numeros
 my_list=[1,2,3,4]
 #obtner el iterador de la lista
-#un interador  es un objeto que nos permite recorrer una coleccion (como una lista)
-#una por uno
+#un interador  es un objeto que nos permite recorrer una coleccion (como una lista)una por uno
 #usar el iterador para acceder a los elementos de la lista
-#la funcion nmext() nos da el siguinet elemento en la coleccion  cada vez que se llama
+#la funcion net() nos da el siguiente elemento en la coleccion  cada vez que se llama
 
 my_iter=iter(my_list)
 print(next(my_iter))
 print(next(my_iter))
 print(next(my_iter))
 print(next(my_iter))
-#iterar cadena de textop usando un iterador
+
+#iterar cadena de texto usando un iterador
 
 text = "Hola mundo"
 #crear un iterador para la cadena
@@ -37,19 +37,21 @@ for char  in iter_text:
        yield 3 # teercer valor que se devuelve cuando  se llam la funcion
        
     for value in my_generator():
-           print(value)
+           print(value)'''
     # fibonacci
     # la serie de Fibonacci hace referencia a que  vamos a obtner un valor sumado
     
-    def fibonacci(limit):
-        a,b=0,1
-        #continaur generanfdo numeros mientras ha  sea menor que el limkite
-        while a<limit:
-         yield a # devolver el valor actual  de "a" pausar la ejecusuion  de la funcion
-         a,b=b,a+b
-         for num in fibonacci(10):
-             print(num)
-        
+def fibonacci(limit):
+    a, b = 0, 1
+    # Continuar generando números mientras 'a' sea menor que el límite
+    while a < limit:
+        yield a  # Devolver el valor actual de 'a' y pausar la ejecución de la función
+        a, b = b, a + b
+
+# Ejemplo de uso: imprimir los números de Fibonacci hasta el límite de 20
+for num in fibonacci(50):
+    print(num)
+
         
             
 
---
+++ uso-de-funciones.py
@@ -0,0 +1,5 @@
+def greet(name,lastname="no tiene apellido"):
+    print("Hola ",name,lastname)
+greet("Ricardo","Maldonado")
+greet("de Jesus")
+greet(lastname= "Maldonado",name="Ricardo")
